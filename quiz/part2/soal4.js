3afd9e541c9e938cf71a881b104702f2a6787999
Ahmad Zaki <ahmdzakiiiy@gmail.com>
2025-11-23 00:11:40 +0700

    push soal 4

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,26 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+secondWord4 = word4.substring(4, 14)
+let secondWordLength = secondWord4.length;
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
+
+thirdWord4 = word4.substring(15, 17)
+let thirdWordLength = thirdWord4.length;
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+
+fourthWord4 = word4.substring(18, 20)
+let fourthWordLength = fourthWord4.length;
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+
+fifthWord4 = word4.substring(21, 25)
+let fifthWordLength = fifthWord4.length;
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
